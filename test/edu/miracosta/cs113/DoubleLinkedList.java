69a40326ba2f3723e8f0e03e3e203f6733925c6f
Lilly McGraw <Lilly McGraw@LAPTOP-PD7MCQOE>
2021-07-12 22:51:26 -0700

    most methods are complete

--- test/edu/miracosta/cs113/DoubleLinkedList.java
+++ test/edu/miracosta/cs113/DoubleLinkedList.java
@@ -3,98 +3,325 @@ import java.util.*;
 
 public class DoubleLinkedList<E> extends AbstractSequentialList<E>
 {  // Data fields
-    	private Node<E> head = null;   // points to the head of the list
-    	private Node<E> tail = null;   //points to the tail of the list
+    	private Node<E> mHead = null;   // points to the head of the list
+    	private Node<E> mTail = null; //points to the tail of the list
     	private int size = 0;    // the number of items in the list
+    	
+    	
   
-  public void add(int index, E obj)
-  { // Fill Here 
-   }
-  public void addFirst(E obj) { // Fill Here 
-	  
-  }
-  public void addLast(E obj) { // Fill Here
-  }
+    public void add(int index, E obj)
+ { // Fill Here 
+	  ListIter iter = new ListIter(index);
+      iter.add(obj);
+      size++;
+ }
+  
+  	public void addFirst(E obj)
+ { 
+	  // Fill Here
+	  Node<E> node = new Node<E> (obj);
+      node.next = mHead;
+      node = mHead;
+      size++;
+ }
+  	
+  	public void addLast(E obj) 
+ { 
+	  // Fill Here
+	  Node<E> node = new Node<E> (obj);
+      node.prev = mTail;
+      mTail.next = node;
+      node = mTail;
+      node.next = null;
+      size++;
+ }
 
-  public E get(int index) 
-  { 	ListIterator<E> iter = listIterator(index); 
-      	return iter.next();
-  }  
-  public E getFirst() { return head.data;  }
-  public E getLast() { return tail.data;  }
+  	private Node<E> getNode(int index) 
+ {
+  		Node<E> node = mHead;
+  		for (int i = 0; i < index && node != null; i++)
+ {
+  			node = node.next;
+ }
+  		return node;
+ }
+  	
+  	public E get(int index) 
+ { 	
+  		if( index < 0 || index >= size)
+ {
+        throw new IndexOutOfBoundsException("Index must be between 0 and" + size());
+ }
+        Node<E> node = getNode(index);
+      	return node.data;   	
+ }  
+  	
+  	public E getFirst()
+ { 
+	  return mHead.data; 
+ }
+  
+  	public E getLast() 
+ {
+	  return mTail.data; 
+ }
 
-  public int size() {  return -1;  } // Fill Here
+  	public int size() 
+ {
+	  return size;
+ }
 
-  public E remove(int index)
-  {     E returnValue = null;
+
+  	public E remove(int index)
+ {      E returnValue = null;
         ListIterator<E> iter = listIterator(index);
         if (iter.hasNext())
-        {   returnValue = iter.next();
+ {   returnValue = iter.next();
             iter.remove();
-        }
-        else {   throw new IndexOutOfBoundsException();  }
+ }
+        else 
+ {  
+        throw new IndexOutOfBoundsException(); 
+ }
         return returnValue;
-  }
+ }
 
-  public Iterator iterator() { return new ListIter(0);  }
-  public ListIterator listIterator() { return new ListIter(0);  }
-  public ListIterator listIterator(int index){return new ListIter(index);}
-  public ListIterator listIterator(ListIterator iter)
-  {     return new ListIter( (ListIter) iter);  }
+  	public Iterator iterator()
+ { 
+  		return new ListIter(0); 
+ }
+  	
+  	public ListIterator listIterator() 
+ {
+  		return new ListIter(0); 
+ }
+  	
+  	public ListIterator listIterator(int index)
+ {
+  		return new ListIter(index);
+ }
+  	
+  	public ListIterator listIterator(ListIterator iter)
+ {    
+  		return new ListIter( (ListIter) iter);
+ }
 
   // Inner Classes
-  private static class Node<E>
-  {     private E data;
+  	private static class Node<E>
+ {      public Node mNext;
+        private E data;
         private Node<E> next = null;
         private Node<E> prev = null;
 
         private Node(E dataItem)  //constructor
-        {   data = dataItem;   }
-  }  // end class Node
+ {  
+        	data = dataItem;  
+ }
+        
+ }      // end class Node
 
-  public class ListIter implements ListIterator<E> 
-  {
-        private Node<E> nextItem;      // the current node
-        private Node<E> lastItemReturned;   // the previous node
-        private int index = 0;   // 
-
-    public ListIter(int i)  // constructor for ListIter class
-    {   if (i < 0 || i > size)
-        {     throw new IndexOutOfBoundsException("Invalid index " + i); }
+  	public class ListIter implements ListIterator<E> 
+ {
+  	    // the current node
+        private Node<E> nextItem;    
+        // the previous node
+        private Node<E> lastItemReturned;   
+        private int index = 0;   
+        
+    // constructor for ListIter class
+    public ListIter(int i) 
+ {      if (i < 0 || i > size)
+	 
+ {    	 
+	 	throw new IndexOutOfBoundsException("Invalid index " + i);
+ }
         lastItemReturned = null;
  
         if (i == size)     // Special case of last item
-        {     index = size;     nextItem = null;      }
+ {   
+        	index = size;     nextItem = null;    
+ }
         else          // start at the beginning
-        {   nextItem = head;
-            for (index = 0; index < i; index++)  nextItem = nextItem.next;   
-        }// end else
-    }  // end constructor
+ {  
+        	nextItem = mHead;
+        for (index = 0; index < i; index++)  nextItem = nextItem.next; 
+            
+ }			// end else
+ }       // end constructor
 
     public ListIter(ListIter other)
-    {   nextItem = other.nextItem;
-        index = other.index;    }
+ {  
+    	nextItem = other.nextItem;
+        index = other.index;    
+ }
 
-    public boolean hasNext() {   return true;    } // Fill Here
+    public boolean hasNext() 
+ {  
+    	return nextItem != null;  
+ } 
+    
     public boolean hasPrevious()
-    {   return false;   } // Fill Here
-    public int previousIndex() {  return 0;    } // Fill Here
-    public int nextIndex() {  return 0;    } // Fill here
-    public void set(E o)  { }  // not implemented
-    public void remove(){}      // not implemented
+ {  
+    	//check if head is null
+    	if (mHead == null)
+    		return false;
+    	
+    	//check that current object exists
+    	return (nextItem == null && size != 0) 
+    			|| nextItem.prev != null;  
+ } 
+    
+    
+    public int previousIndex() 
+ {  
+    	return index-1;   
+ } 
+    
+    public int nextIndex() 
+ { 
+    	return index;    
+ } 
+    
+    public void set(E o) 
+ { 
+    	 if (lastItemReturned != null) 
+ {
+             lastItemReturned.data = o;
+ } 
+         else 
+ {
+             throw new IllegalStateException();
+ }	
+ } 
+    
+    public void remove()
+ {
+    	  // if list is empty or no item was returned
+        if (isEmpty() || lastItemReturned == null)
+ {
+            throw new IllegalStateException();
+ }
+        else
+ {
+            // only one node in list
+            if (size() == 1)
+ {
+                mHead = null;
+ }
 
+            // first node in list
+            else if (lastItemReturned.prev == null)
+ {
+                mHead = nextItem;
+                lastItemReturned.next.prev = null;
+ }
+            // last node in list
+            else if (lastItemReturned.next == null)
+ {
+                lastItemReturned.prev.next = null;
+ }
+            else // everything else in between
+ {
+                lastItemReturned.prev.next = lastItemReturned.next;
+                lastItemReturned.next.prev = lastItemReturned.prev;
+ }
+            size--;
+            lastItemReturned = null;
+ }
+ }      
+
+    //move the iterator forward and return the next item
     public E next()
-    {  
-        return lastItemReturned.data; // Fill Here 
-    }
+ {  
+    	//if there is no next item, method is invalid
+    	if (!hasNext())
+ {
+    		throw new NoSuchElementException();
+ }
+    	// previous is moved up
+    	lastItemReturned = nextItem;
+    	// next item is moved up one spot
+    	nextItem = nextItem.next;
+    	// increases index
+    	index++;
+        return lastItemReturned.data; 
+ }
 
     public E previous() 
-    {  return lastItemReturned.data; // Fill Here 
-    }
+ {  
+    	if (!hasPrevious())
+ {
+    		throw new NoSuchElementException();
+ }
+    	
+    	if(nextItem == null)
+ {
+    		//iterator is the past element
+    		nextItem = mTail;
+ }
+    	  
+    	else
+ {
+    		nextItem = nextItem.prev;
+ }
+    	
+    	lastItemReturned = nextItem;
+    	index--;
+    	return lastItemReturned.data;
+ }
 
-    public void add(E obj) {
-
-    // Fill Here
-    }
+    public void add(E obj)
+ {
+    	 Node<E> newNode;
+    	 
+    	if (mHead == null)
+ {
+    		// add to an empty list
+    		mHead = new Node<E>(obj);
+    		mTail = mHead;
+ }
+    	
+    	else if
+    	//insert at head
+    	(nextItem == mHead)
+ {
+    		//create a new node
+    		newNode = new Node<E>(obj);
+    		//link it to the next item
+    		newNode.next = nextItem;
+    		//link nextItem to the new node
+    		nextItem.prev = newNode;
+    		// new node is now the head
+    		mHead = newNode;
+ }	
+    	 else if 
+    	 // insert at tail
+    	 (nextItem == null)
+ {
+    		 //create a new node
+    		 newNode = new Node<E>(obj);
+    		 // link the tail to the new node
+    		 mTail.next = newNode;
+    		 // link the new node to the tail
+    		 newNode.prev = mTail;
+    		 // the new node is the new tail
+    		 mTail = newNode;
+ }
+    	 
+    	 else 
+ {
+    		 //insert into the middle
+    		 //create a new node
+    		 newNode = new Node<E>(obj);
+    		 //link it to the nextItem.prev
+    		 newNode.prev = nextItem.prev;
+    		 nextItem.prev.next = newNode;
+    		 // link it to the nextItem
+    		 newNode.next = nextItem;
+    		 nextItem.prev = newNode;
+ }
+    	 
+    	
+    	}
   }// end of inner class ListIter
 }// end of class DoubleLinkedList
